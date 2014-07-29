6f6c705e04bf77e926dfca0e9d0757cf2f5044cf
Jake Boylan <jpb158@gmail.com>
2014-07-28 23:44:54 -0400

    Adding Bower Gulp task; Updating asset file locations;

--- config.rb
+++ config.rb
@@ -1,12 +1,15 @@
-# Require any additional compass plugins here.
+# Plugins
 require 'autoprefixer-rails'
 
-# Set this to the root of your project when deployed:
+
+
 http_path = "/"
 css_dir = "site/assets/dist/css"
 sass_dir = "site/assets/src/sass"
 images_dir = "site/assets/src/img"
 javascripts_dir = "site/assets/src/js"
+
+
 
 # Autoprefix
 on_stylesheet_saved do |filename|
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,24 @@
 
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 
 var paths = {
   scripts: 'site/assets/src/js/**/*.js',
   images: 'site/assets/src/img/**/*'
 };
+
+
+gulp.task('bower', function(cb) {
+  gulp.src('./bower_components/modernizr/modernizr.js')
+    .pipe(gulp.dest('./site/assets/src/js/lib'));
+
+  gulp.src('./bower_components/normalize.css/normalize.css')
+    .pipe(rename('normalize.scss'))
+    .pipe(gulp.dest('./site/assets/src/sass/lib'));
+});
+
+
+// gulp.src('./bower_components/normalize.css/normalize.css')
+//   .pipe(rename('))
+
+gulp.task('default', ['bower']);
